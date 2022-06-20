011e067ddb7307e33b8c7ac7cb19511880d0aedd
Madan Lal <madanlal.suther@gmail.com>
2022-06-21 00:57:05 +0500

    added more unit testing test cases

--- Additional_Programs/Unit Testing/sample/__test__/unitTesting.test.js
+++ Additional_Programs/Unit Testing/sample/__test__/unitTesting.test.js
@@ -1,3 +1,31 @@
 test('It adds 2 numbers together', () => {
     expect(1+3).toBe(4);
 })
+
+test('Check number is greater than or not', () => {
+    let a = 7;
+    expect(a).toBeGreaterThan(8);
+})
+
+test('Check number is less than or not', () => {
+    let a = 7;
+    expect(a).toBeLessThan(8);
+})
+
+test('It compare objects', () => {
+    let obj1 = {name: "Madan"};
+    let obj2 = {name: "Madan"};
+    expect(obj1).toBe(obj1);
+})
+
+test('Check it is not_a_number', () => {
+    let a = 7-'A';
+    expect(a).toBeNaN(); 
+})
+
+
+
+
+
+
+
